9b709623f753465d5e965002b18a5d8c82179d71
DESKTOP-B7QLANU\leech <alex.lee@loop.colum.edu>
2017-12-10 19:20:10 -0600

    remove old broken project

--- HanoiSim/Source/UE4HanoiSim/UE4HanoiSim.cpp
+++
@@ -1,5 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "UE4HanoiSim.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE4HanoiSim, "UE4HanoiSim" );
